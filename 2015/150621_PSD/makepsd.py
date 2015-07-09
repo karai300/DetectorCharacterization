1a48bf6c5398c74d1f71c590dbefcd3c81510bc2
Eve Chase <eachase@email.wm.edu>
2015-07-08 17:20:00 -0700

    Calculate 1st-4th order stats, makes histograms -EC

--- 2015/150621_PSD/makepsd.py
+++ 2015/150621_PSD/makepsd.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import readligo as rl
@@ -13,8 +14,8 @@ def get_freq_statistics(freq_selected, psds):
 
         Given a set of PSDs computed from several frequency chunks of the 
         same data set, the spread of strain / sqrt(Hz) values are compared 
-        for all PSDs at a selected frequency. Mean and standard deviation 
-        are returned.
+        for all PSDs at a selected frequency. Mean, variance, skewness, and
+	kurtosis are returned.
         
         freq_selected --  a frequency in Hz at which the PSDs will be compared
 
@@ -24,8 +25,11 @@ def get_freq_statistics(freq_selected, psds):
         mean -- average of the strain / sqrt(Hz) a.k.a. psd_h values at 
         the selected frequency
 
-        std -- standard deviation of the aforementioned values
+        var -- variance of the aforementioned values
 
+	skew -- third order statistics
+
+	kurt -- fourth order statistics
         """
 
         # Store psd_h for selected frequncy in each PSD
@@ -39,11 +43,39 @@ def get_freq_statistics(freq_selected, psds):
                                 # !!! - awkward to have list of arrays
 
 
-        # Calculate standard deviation and mean of corresponding psd_h
-        mean = np.average(psd_h)
-        std = np.std(psd_h)
-        return mean, std
+        # Calculate moments of power for corresponding psd_h
+	n, min_max, mean, var, skew, kurt = stats.describe(psd_h)       
+ 
+	return mean, var, skew, kurt
 
+
+def plot_histogram(freq_selected, psds):
+	
+	"""
+	Plot a histogram of various PSD values at one selected frequency.
+
+	freq_selected -- selected frequency in Hz
+
+	psds -- a list of tuples, with one tuple representing the frequency
+	and strain / sqrt(Hz) values for one PSD
+	"""
+
+	# Store psd_h for selected frequency in each PSD
+	psd_h = []	
+
+	# Find corresponding PSD value for each selected frequency
+	for psd in psds:
+		for index, freq in enumerate(psd[0]):
+			if freq == freq_selected:
+				psd_h.append(psd[1][index])
+
+	# Plot histogram
+	plt.figure(5)
+	plt.hist(psd_h, 20)
+	plt.xlabel('PSD (strain / (Sqrt(Hz))')
+	plt.ylabel('Count')
+	plt.savefig('histogram.pdf')
+	
 
 def plot_timeseries(time_seg, strain_seg):
 	
@@ -122,7 +154,7 @@ time_seg = time[seglist[0]][:(length * fs)]
 #plot_timeseries(time_seg, strain_seg)
 
 # Calculate a PSD
-pxx, freqs = plot_psd(time_seg, strain_seg, fs, False)
+pxx, freqs = plot_psd(time_seg, strain_seg, fs, True)
 
 # Check the PSD through Parseval's Theorem
 total_power = np.sum(pxx * (freqs[1] - freqs[0])) # Integrate PSD over all freqs
@@ -135,15 +167,21 @@ print 'total power: ', total_power, '\n'
 
 # Select data length based on closest power of two which will give 
 # approximately the desired number of segments
-num_segments = 80
+num_segments = 63
 approx_seg_length = len(strain_seg) / num_segments
 seg_length = int(pow(2, np.ceil(np.log(approx_seg_length)/np.log(2))))
 print 'seg length: ', seg_length
 print 'strain length: ', len(strain_seg)
 
 # !!! - Change NFFT based on number of segments. 
-nfft = int(seg_length)
-#nfft = 1024
+if num_segments <= 20:
+	nfft = int(seg_length / 2)
+	pad = nfft * 4
+else:
+	nfft = int(seg_length)
+	pad = nfft * 2
+	
+#nfft = 4096
 print 'nfft: ', nfft
 # 4096 works for 1 seg, 10 segs, 20 segs
 # 2048 looks ok for 20 segs but not great
@@ -161,7 +199,7 @@ while (max_index + seg_length) < len(strain_seg):
 	# Calculate each PSD
 	pxx, freqs = mlab.psd(
 		strain_seg[min_index:max_index], Fs=fs, NFFT=nfft, 
-		noverlap=nfft/2)
+		noverlap=nfft/2, pad_to=pad)
 	plt.loglog(freqs, np.sqrt(np.abs(pxx)))
 
 	# Store list of tuples for PSD values
@@ -175,27 +213,39 @@ plt.ylim([1e-26, 1e-16])
 #plt.savefig('manyPSDs.pdf')
 
 # Plot PSD statistics for every frequency in range
-averages = []
-many_stds = []
+many_mean = []
+many_std = []
+many_skew = []
+many_kurt = []
 for freq in freqs:
-	(mean, std) = get_freq_statistics(freq, psds)
-	averages.append(mean)
-	many_stds.append(std)
+	mean, var, skewness, kurtosis = get_freq_statistics(freq, psds)
+	many_mean.append(mean)
+	many_std.append(np.sqrt(var))
 	#if mean - 2*std < 0:
 		#print freq, mean, mean - 2*std, std
-averages = np.asarray(averages)
-many_stds = np.asarray(many_stds)
+many_mean = np.asarray(many_mean)
+many_std = np.asarray(many_std)
+many_skew = np.asarray(many_skew)
+many_kurt = np.asarray(many_kurt)
 plt.figure(4)
-plt.loglog(freqs, averages, lw=1, color='red', label='Mean')
-plt.loglog(freqs, averages + 2*many_stds, lw=1, color='blue', label='+2 sigma')
-plt.loglog(freqs, averages - 2*many_stds, lw=1, color='green', label='-2 sigma')
+mean = plt.loglog(freqs, many_mean, lw=1, color='red')
+std4 = plt.fill_between(
+	freqs, many_mean, many_mean + 4*many_std, facecolor='yellow')
+std2 = plt.fill_between(freqs, many_mean, many_mean + 2*many_std, facecolor='blue')
+#plt.loglog(freqs, many_mean + 2*many_std, lw=1, color='blue', label='+2 sigma')
+#plt.loglog(freqs, many_mean - 2*many_std, lw=1, color='green', label='-2 sigma')
 plt.grid('on')
-plt.legend()
+#plt.legend([mean, std4, std2], ['Mean', '4 std', '2std'])
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('PSD (strain / Sqrt(Hz))')
 plt.title('Average of several PSDs for L1 data starting at GPS ' + str(time_seg[0])) 
 plt.ylim([1e-26, 1e-16])
-#plt.savefig('psd_statistics.pdf')
+plt.savefig('psd_statistics.pdf')
+
+# Display histogram for a chosen frequency (in Hz as first parameter)
+plot_histogram(500, psds)
+print get_freq_statistics(freq, psds)
+	
 
 plt.show()
 
